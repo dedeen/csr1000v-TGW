bea300fbdde90f803df39938a49c27968b9a818c
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-05-07 22:01:09 -0500

    Delete reclone.sh

--- terraform/reclone.sh
+++
@@ -1,9 +0,0 @@
-# Script to reset local git repo as I am editing in test env. 
-#   This will abandone local changes from my scratch directory and 
-#   put a new version from github
-# dan@dsblue.net  
-#
-
-git fetch 
-git reset --hard HEAD
-git merge '@{u}'
