f6ebce4bcdaa1a00176d98f404dd198f29883c42
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-05-07 21:33:20 -0500

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -12,6 +12,14 @@ variable "app_vpcs" {
 			vpc_subnets		= 	["10.104.0.0/24","10.104.1.0/24","10.104.128.0/24","10.104.129.0/24"]
 			subnet_names		= 	["vpc104-az1-pub","vpc104-az1-priv","vpc104-az2-pub","vpc104-az2-priv"]
 		},
+		vpc105 = {		
+			map_key			= 	"vpc105"
+			region_dc		= 	"VPC-105"
+			cidr			= 	"10.105.0.0/16"
+			az_list			= 	["us-west-2a","us-west-2a","us-west-2b","us-west-2b"]
+			vpc_subnets		= 	["10.105.0.0/24","10.105.1.0/24","10.105.128.0/24","10.105.129.0/24"]
+			subnet_names		= 	["vpc105-az1-pub","vpc105-az1-priv","vpc105-az2-pub","vpc105-az2-priv"]
+		},
 		
 	}   
 }
