7fbaa67986022a81ed386dcda01ef5823748fca2
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-05-07 21:32:11 -0500

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,25 +1,17 @@
-/*  Terraform variables defined here. 
-      Dan Edeen, dan@dsblue.net, 2023 
-*/
-
 # VPC parms, can build mutiple by passing in via this map 
 variable "app_vpcs" {
 	description = "VPC Variables"
 	type		= map(any)
 
 	default = {
-		secvpc = {		
-			map_key			= 	"secvpc"
-			region_dc		= 	"Sec01-VPC"
-			cidr			= 	"10.100.0.0/16"
-			#az_list		= 	["us-west-2a","us-west-2a","us-west-2a","us-west-2a","us-west-2b","us-west-2b","us-west-2b","us-west-2b"]
-			#vpc_subnets		= 	["10.100.0.0/24","10.100.1.0/24","10.100.2.0/24","10.100.3.0/24","10.100.64.0/24","10.100.65.0/24","10.100.66.0/24","10.100.67.0/24"]
-			#subnet_names		= 	["sec-az1-pub","sec-az1-priv","sec-az1-dmz","sec-az1-mgmt","sec-az2-pub","sec-az2-priv","sec-az2-dmz","sec-az2-mgmt"]
-			az_list			= 	["us-west-2a","us-west-2a","us-west-2a","us-west-2a"]
-			vpc_subnets		= 	["10.100.0.0/24","10.100.1.0/24","10.100.2.0/24","10.100.3.0/24"]
-			subnet_names	= 		["sec-az1-pub","sec-az1-priv","sec-az1-dmz","sec-az1-mgmt"]
-		
-		}
+		vpc104 = {		
+			map_key			= 	"vpc104"
+			region_dc		= 	"VPC-104"
+			cidr			= 	"10.104.0.0/16"
+			az_list			= 	["us-west-2a","us-west-2a","us-west-2b","us-west-2b"]
+			vpc_subnets		= 	["10.104.0.0/24","10.104.1.0/24","10.104.128.0/24","10.104.129.0/24"]
+			subnet_names		= 	["vpc104-az1-pub","vpc104-az1-priv","vpc104-az2-pub","vpc104-az2-priv"]
+		},
 		
 	}   
 }
